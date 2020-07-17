37dc0ce1ae75085c51976f52d9a5fcc696116b16
Anna Lee <annaleeart@yandex.ru>
2020-07-17 16:09:54 +0300

    Fixed errors. Configured webpack for 2 pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,74 +7,78 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-        savednews: './src/savednews.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name]/[name].[contenthash].js'
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                  (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                  'css-loader',
-                  'postcss-loader'
-                ]
-            },
-            {
-              test: /\.(gif|png|jpe?g|ico|svg)$/i,
-              use: [
-                  'file-loader?name=./images/[name].[ext]',
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                        bypassOnDebug: true,
-                        disable: true,
-                    },
-                  },
-                ],
-          },
-          {
-              test: /\.(eot|ttf|woff|woff2)$/,
-              loader: 'file-loader?name=./vendor/[name].[ext]'
-          }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({ filename: '[name]/[name].[contenthash].css' }),
-        new OptimizeCssAssetsPlugin({
-          assetNameRegExp: /\.css$/g,
-          cssProcessor: require('cssnano'),
-          cssProcessorPluginOptions: {
-                  preset: ['default'],
-          },
-          canPrint: true
-        }),
-        new HtmlWebpackPlugin({
-          inject: true,
-          template: './src/index.html',
-          chunks: ['main'],
-          filename: './index.html'
-      }),
-        new HtmlWebpackPlugin({
-          inject: true,
-          template: './src/savednews.html',
-          chunks: ['savednews'],
-          filename: './savednews.html'
-      }),
-        new WebpackMd5Hash(),
-        new webpack.DefinePlugin({
-          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      })
+  // файл, куда первым делом заглянет «Вебпак»
+  entry: {
+    main: './src/index.js',
+    savednews: './src/savednews/savednews.js',
+  },
+  // файл, куда складывать собранные файлы
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              // загрузчик loader — функция, которая трансформирует исходный код
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.css$/i,
+            use: [
+              (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+              'css-loader',
+              'postcss-loader'
+            ]
+        },
+        {
+            test: /\.(gif|png|jpe?g|ico|svg)$/i,
+            use: [
+              'file-loader?name=./images/[name].[ext]',
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                    bypassOnDebug: true,
+                    disable: true,
+                },
+              },
+            ],
+      },
+      {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          loader: 'file-loader?name=./vendor/[name].[ext]'
+      }
     ]
+  },
+  // plugin - модуль, который преобразует код при компиляции и сборке в целом
+  plugins: [
+    new MiniCssExtractPlugin({filename: '[name].[contenthash].css'}),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+      filename: './index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/savednews/savednews.html',
+      filename: './savednews.html',
+      chunks: ['savednews'],
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
 };
